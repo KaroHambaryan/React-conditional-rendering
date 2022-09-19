74a5c286e52c5fb0792c6e679971fa111dcf35da
vision <karohambaryan@gmail.com>
2022-09-19 20:17:13 +0400

    Կլասսային կոմպոնենտով վիճակի փոփոխություն

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+
+class App extends React.Component {
+	state = { num: 0 };
+
+	countUp = () => {
+		this.setState(current => {
+			return {
+				num: current.num + 1
+			};
+		})
+	};
+	render() {
+	
+		console.log("render",this.a);
+		const { num } = this.state
+		return (
+			<div >
+				<h1>number - {num}</h1>
+				<button onClick={this.countUp}>UP</button>
+			</div>
+		);
+	}
 }
 
 export default App;
