a936a9a185ab0fb751d574f64e8d6352daf5e396
vision <karohambaryan@gmail.com>
2022-09-19 21:49:57 +0400

    ուսումնասիրում ենք useEffect-ի կախվածությունները

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import { useEffect, useState } from "react";
 
 function App() {
-	const [num, setNum] = useState(0);
+	const [count, setCount] = useState(0);
+	const [deg, setDeg] = useState(0);
 
 	useEffect(() => {
-		console.log('redy');
-	},[])
+		console.log('worked...');
+	})
 
-	const countUp = () => {
-		setNum(num => ++num)
+	const chengeFirst = () => {
+		setCount(count => count + 1)
 	}
-
-	const countDown = () => {
-		setNum(num => --num)
+	const chengeSecond = () => {
+		setDeg(deg => deg + 10)
 	}
-	
 	return (
 		<div >
-				<h1>number-({num})</h1>
-			<button onClick={countUp}>UP</button>
-			<button onClick={countDown}>Down</button>
-			</div>
-		);
-	
+			<h2>Current-{count}</h2>
+			<h2>Current-{deg}</h2>
+			<button onClick={chengeFirst}>chenge first</button>
+			<button onClick={chengeSecond}>chenge second</button>
+		</div>
+	);
+
 }
 
 export default App;
