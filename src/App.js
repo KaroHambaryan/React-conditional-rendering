573fc2faef32f41383fa5eaabfae6b2f3174ce76
vision <karohambaryan@gmail.com>
2022-09-20 16:22:12 +0400

    Start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import Playlist from "./Components/Playlist";
-import Song from "./Components/Song";
-
 import './App.css';
+
 function App() {
 
 	return (
 		<div>
-			<Playlist>
-				<Song
-					title="I have Nothing"
-					singer="Whitnney Houston"
-					duration="3"
-				/>
-				<Song
-					title="Image"
-					singer="John Lenon"
-					duration="5.3"
-				/>
-				<Song
-					title="Yesterday"
-					singer="The Beatels"
-					duration="4.2"
-				/>
-			</Playlist>
+			
 		</div>
 	);
 
