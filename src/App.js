6d3f8cb61fcbc14eab0587ee2394defc03c18ccd
vision <karohambaryan@gmail.com>
2022-09-20 16:09:29 +0400

    Ուսումնասիրեցինք կոմպոնենտների կոմպոզիցիան

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
+import Playlist from "./Components/Playlist";
 import Song from "./Components/Song";
 
 import './App.css';
 function App() {
-	
+
 	return (
-		<div className="App">
-			<h1>Playlist</h1>
-			<Song
-				title="I have Nothing"
-				singer="Whitnney Houston"
-				duration="3"
-			/>
-			<Song
-				title="Image"
-				singer="John Lenon"
-				duration="5.3"
-			/>
-			<Song
-				title="Yesterday"
-				singer="The Beatels"
-				duration="4.2"
-			/>
+		<div>
+			<Playlist>
+				<Song
+					title="I have Nothing"
+					singer="Whitnney Houston"
+					duration="3"
+				/>
+				<Song
+					title="Image"
+					singer="John Lenon"
+					duration="5.3"
+				/>
+				<Song
+					title="Yesterday"
+					singer="The Beatels"
+					duration="4.2"
+				/>
+			</Playlist>
 		</div>
 	);
 
